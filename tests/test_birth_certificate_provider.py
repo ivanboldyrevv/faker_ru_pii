6820139090620bf2805c1078a3f8c4995f82b055
ivan <1ibah1ivan1@gmail.com>
2026-01-09 22:33:08 +0400

    fix test name

--- tests/test_birth_certificate_provider.py
+++ tests/test_birth_certificate_provider.py
@@ -7,7 +7,7 @@ import pytest
     ("birth_certificate_series", r"(I|II|III|IV|V|VI|VII|VIII|IX|X)-[А-Я]{2}"),
     ("birth_certificate_full", r"(I|II|III|IV|V|VI|VII|VIII|IX|X)\s?\-?[А-Я]{2} \d{6}")
 ])
-def test_passport_format(faker, num_samples, method, pattern):
+def test_birth_certificate_format(faker, num_samples, method, pattern):
     func = getattr(faker, method)
     for _ in range(num_samples):
         value = func()
